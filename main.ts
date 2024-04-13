97bf1d142d913242e498309c43cbf05e4f8daa37
wardaarsalanS <164078434+wardaarsalanS@users.noreply.github.com>
2024-04-13 10:47:12 +0500

    ATM Machine Simulation
    The ATM Machine Simulation is a software-based representation of an Automated Teller Machine (ATM), enabling users to conduct various banking transactions securely and conveniently.
    
    Features:
    
    User Authentication:
    
    Secure login using account number and PIN.
    Transaction Options:
    
    Withdraw cash, deposit cash, check balance, and transfer funds.
    Security Measures:
    
    PIN verification, timeout feature, transaction limits.
    Transaction Logging:
    
    Records transactions for auditing provide receipts.
    Implementation:
    Developed using Java/Python/C++, with file systems or databases for data management.
    
    Benefits:
    
    Convenient access to banking services.
    Enhanced security through authentication and logging.
    User-friendly interface for seamless transactions.
    The ATM Machine Simulation provides a realistic and efficient means of conducting electronic banking transactions.

---
+++ main.ts
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+
+let mybalance =10000;// Doller
+let mypin = 3344;
+
+let pinanswer = await inquirer.prompt(
+[
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number",
+    }
+]
+);
+// 3344 == 3344 -false
+if (pinanswer.pin === mypin){
+    console.log("correct pin code!!!");
+}
+    let operationans =await inquirer.prompt(
+        [
+            {
+                name: "warda",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw", "check blance"]
+            }
+        ]
+    );
+   
+    console.log(operationans);
+
+    if(operationans.operation == "withdraw") {
+        let amountans = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number",
+                }
+            ]
+        );
+        // =, -=, +=
+        mybalance -= amountans.amount;
+        console.log ("your remaining balance is: " + mybalance)
+
+    }else if(operationans.operation === "check balance"){
+        console.log("your blance is: " + mybalance)
+    }
